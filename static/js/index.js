baf118cecfc074f92a88c43ef804b6cc86a40e25
Ricards <G00314715@gmit.ie>
2018-06-14 18:10:54 +0100

    Minor Change

--- static/js/index.js
+++ static/js/index.js
@@ -53,11 +53,17 @@ var Fake = [
 	'Hi there, I\'m Cortana.',
 	'Command accepted, executing order 66.',
 	'We\'re no strangers to love..',
+	'You know the rules and so do I',
 	'A full commitment\'s what I\'m thinking of',
+	'You wouldn\'t get this from any other AI',
 	'I just wanna tell you how I\'m feeling',
+	'Gotta make you understand',
 	'Never gonna give you up ',
+	'Never gonna let you down',
 	'Never gonna run around and desert you',
+	'Never gonna make you cry',
 	'Never gonna say goodbye',
+	'Never gonna tell a lie and hurt you',
 	'Shutting down...'
 ]
 
